2fea64f14614bad4758fb9b74e5a22da762df5ad
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-11-01 05:09:31 +0000

    Cloned app

---
+++ client_code/lendor_registration_form/star_1_lendor_rgistration_form/lendorhomepage/__init__.py
@@ -0,0 +1,66 @@
+from ._anvil_designer import lendorhomepageTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+class lendorhomepage(lendorhomepageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_11_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box1")
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("bank_users.user_form")
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("bank_users.main_form")
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box2")
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box3")
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box4")
+
+  def button_9_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box5")
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box6")
+
+  def button_6_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box7')
+
+  def button_7_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box8")
+
+  def button_8_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box9")
+
+  def button_10_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box10")
+
---
+++ client_code/lendor_registration_form/star_1_lendor_rgistration_form/lendorhomepage/box9/__init__.py
@@ -0,0 +1,53 @@
+from ._anvil_designer import box9Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+class box9(box9Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage")
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box1")
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box2")
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box3")
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box4")
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box5")
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box6")
+
+  def link_7_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box7")
+
+  def link_8_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box8")
+
+  def link_9_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box10")
