869cd5e0302cd0091f62a4cfde47724aa7149600
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-11-01 11:17:14 +0000

    Edited lendor_registration_form.star_1_lendor_rgistration_form.lendorhomepage.box2

--- client_code/lendor_registration_form/star_1_lendor_rgistration_form/lendorhomepage/box2/__init__.py
+++ client_code/lendor_registration_form/star_1_lendor_rgistration_form/lendorhomepage/box2/__init__.py
@@ -14,18 +14,22 @@ class box2(box2Template):
         self.init_components(**properties)
 
         # Query your data
-        data = app_tables.view_bor_loan_requests.get()   # Assuming you want the first record
+        data = app_tables.view_bor_loan_requests.get()  # Retrieve a single record
 
         if data:
-            initial_commitment = data['initial_commitment']
-            top_up = data['top_up']
-            total_loan_disbursed = data['total_loan_disbursed']
+            initial_commitment = data('Initial Commitment')
+            top_up = data('Top Up')
+            total_loan_disbursed = data('Total Loan Disbursed')
 
             available_balance = initial_commitment + top_up - total_loan_disbursed
 
-            self.output_lbl.text = (available_balance)
+            self.output_lbl.text = str(available_balance)
         else:
             self.output_lbl.text = "No data available"
+
+    # ... (your other methods)
+
+    # ... (your other methods)
 
     # ... (your other methods)
 
